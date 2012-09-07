a407e5d0a94269d0f38b877536996117ba93e570
Roger.LC Chuang <pharspher@gmail.com>
2012-09-07 09:47:16 +0800

    Making text in menu page being touchable

--- src/org/ph/game/gamestate/MenuGameState.java
+++ src/org/ph/game/gamestate/MenuGameState.java
@@ -1,10 +1,12 @@
 package org.ph.game.gamestate;
 
+import org.andengine.engine.Engine;
 import org.andengine.engine.camera.Camera;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
 import org.andengine.entity.text.Text;
 import org.andengine.entity.text.TextOptions;
+import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.font.BitmapFont;
 import org.andengine.util.HorizontalAlign;
 import org.ph.game.testgame.GameContext;
@@ -49,19 +51,38 @@ public class MenuGameState extends BaseGameState {
 		mScene = new Scene();
 		mScene.setBackground(new Background(1.0f, 1.0f, 1.0f));
 
-		final Camera camera = mGameContext.getEngine().getCamera();
+		final Engine engine = mGameContext.getEngine();
+		final Camera camera = engine.getCamera();
 		int itemInterval = 
 				(int)(camera.getHeight() / (mMenuItemList.length + 1));
 
 		for (int i = 0; i < mMenuItemList.length; i++) {
-			Text itemText =  new Text(0, 
+			Text itemText = new Text(0, 
 					(i + 1) * itemInterval - mBitmapFont.getLineHeight() / 2, 
 					mBitmapFont, mMenuItemList[i], 
 					new TextOptions(HorizontalAlign.CENTER), 
-					mGameContext.getEngine().getVertexBufferObjectManager());
+					mGameContext.getEngine().getVertexBufferObjectManager()) {
+				@Override
+				public boolean onAreaTouched(final TouchEvent event, final float x, final float y) {
+					switch (event.getAction()) {
+					case TouchEvent.ACTION_DOWN:
+						mGameContext.getGameActivity().finish();
+						return true;
+					default:
+						return false;
+					}
+				}
+			};
 
+			mScene.registerTouchArea(itemText);
 			itemText.setScale(2);
 			itemText.setX(camera.getWidth() / 2 - (itemText.getWidth()) / 2);
+
+			/*
+			ButtonSprite btn = new ButtonSprite(0, 
+					(i + 1) * itemInterval - mBitmapFont.getLineHeight() / 2,
+					engine.
+			*/
 
 			mScene.attachChild(itemText);
 		}
