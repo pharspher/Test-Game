d34182f3644c164915f64ecbc932960fa99b3c71
Roger.LC Chuang <pharspher@gmail.com>
2012-09-09 00:11:21 +0800

    Add tiled sprite button class

---
+++ src/org/ph/game/widget/AnimatedSpriteButton.java
@@ -0,0 +1,59 @@
+package org.ph.game.widget;
+
+import org.andengine.entity.sprite.ButtonSprite;
+import org.andengine.entity.text.Text;
+import org.andengine.entity.text.TextOptions;
+import org.andengine.input.touch.TouchEvent;
+import org.andengine.opengl.font.IFont;
+import org.andengine.opengl.texture.region.ITiledTextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+import org.andengine.util.HorizontalAlign;
+
+public abstract class AnimatedSpriteButton extends ButtonSprite {
+	//private static final String TAG = "AnimatedSpriteButton";
+
+	private boolean mIsPressed = false;
+
+	public AnimatedSpriteButton(float pX, float pY,
+			ITiledTextureRegion pTiledTextureRegion,
+			VertexBufferObjectManager pVertexBufferObjectManager) {
+		super(pX, pY, pTiledTextureRegion, pVertexBufferObjectManager);
+	}
+
+	public void setText(String pText, IFont pFont) {
+		Text text = new Text(0, 0, pFont, pText, 
+				new TextOptions(HorizontalAlign.CENTER), 
+				getVertexBufferObjectManager());
+		text.setPosition((getWidth() - text.getWidth()) / 2, 
+				(getHeight() - text.getHeight()) / 2);
+		attachChild(text);
+	}
+
+	@Override
+	public boolean onAreaTouched(TouchEvent pSceneTouchEvent,
+			float pTouchAreaLocalX, float pTouchAreaLocalY) {
+		switch (pSceneTouchEvent.getAction()) {
+		case TouchEvent.ACTION_DOWN:
+			mIsPressed = true;
+			setCurrentTileIndex(1);
+			return true;
+		case TouchEvent.ACTION_MOVE:
+			if (pTouchAreaLocalX <= 0 || pTouchAreaLocalX >= getWidth() || 
+			    pTouchAreaLocalY <= 0 || pTouchAreaLocalX >= getHeight()) {
+				mIsPressed = false;
+				setCurrentTileIndex(0);
+			}
+			return true;
+		case TouchEvent.ACTION_UP:
+			setCurrentTileIndex(0);
+			if (mIsPressed) {
+				mIsPressed = false;
+				onClick();
+			}
+			return true;
+		};
+		return super.onAreaTouched(pSceneTouchEvent, pTouchAreaLocalX, pTouchAreaLocalY);
+	}
+
+	public abstract void onClick();
+}
