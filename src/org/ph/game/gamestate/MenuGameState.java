ca690ee0518f30fad753f28fd62eec81e8cba81f
Roger.LC Chuang <pharspher@gmail.com>
2012-09-12 01:02:03 +0800

    1. Add temprorary tiled button sprite asset. 2. Use AnimatedSpriteButton to construct game menu.

--- src/org/ph/game/gamestate/MenuGameState.java
+++ src/org/ph/game/gamestate/MenuGameState.java
@@ -4,12 +4,18 @@ import org.andengine.engine.Engine;
 import org.andengine.engine.camera.Camera;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
-import org.andengine.entity.text.Text;
-import org.andengine.entity.text.TextOptions;
-import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.font.BitmapFont;
-import org.andengine.util.HorizontalAlign;
+import org.andengine.opengl.texture.TextureManager;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.atlas.bitmap.BuildableBitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.source.IBitmapTextureAtlasSource;
+import org.andengine.opengl.texture.atlas.buildable.builder.BlackPawnTextureAtlasBuilder;
+import org.andengine.opengl.texture.atlas.buildable.builder.ITextureAtlasBuilder.TextureAtlasBuilderException;
+import org.andengine.opengl.texture.region.TiledTextureRegion;
 import org.ph.game.testgame.GameContext;
+import org.ph.game.widget.AnimatedSpriteButton;
 
 import android.util.Log;
 
@@ -21,10 +27,20 @@ public class MenuGameState extends BaseGameState {
 	private Scene mScene;
 	private BitmapFont mBitmapFont;
 
-	private GameContext mGameContext;
+	private BuildableBitmapTextureAtlas mBitmapTextureAtlas;
+	private TiledTextureRegion mTiledButtonRegion;
 
-	private static String[] mMenuItemList = new String[] {
+	private GameContext mGameContext;
+	private Engine mEngine;
+
+	private static final String[] mMenuItemList = new String[] {
 		"New Game", "Resume Game", "Options", "Quit"
+	};
+
+	private MenuItemClickListener[] mMenuItemClickListener;
+
+	private interface MenuItemClickListener {
+		public void onClick();
 	};
 
 	public MenuGameState(GameContext context) {
@@ -37,11 +53,62 @@ public class MenuGameState extends BaseGameState {
 		Log.d(TAG, "onCreateResource()");
 
 		mGameContext = getGameContext();
+		mEngine = mGameContext.getEngine();
+		TextureManager textureManager = mEngine.getTextureManager();
+
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("sprite/");
+		mBitmapTextureAtlas = 
+				new BuildableBitmapTextureAtlas(textureManager, 
+						300, 200, TextureOptions.NEAREST);
+
+		mTiledButtonRegion = BitmapTextureAtlasTextureRegionFactory.
+				createTiledFromAsset(mBitmapTextureAtlas, 
+						mGameContext.getGameActivity(), 
+						"tiled_button_sprite0.png", 1, 2);
+
+		try {
+			mBitmapTextureAtlas.build(
+					new BlackPawnTextureAtlasBuilder<IBitmapTextureAtlasSource, 
+					BitmapTextureAtlas>(0, 0, 0));
+		} catch (TextureAtlasBuilderException e) {
+			e.printStackTrace();
+		}
+		mBitmapTextureAtlas.load();
 
 		mBitmapFont = 
 		new BitmapFont(mGameContext.getEngine().getTextureManager(),
 				mGameContext.getAssets(), BITMAP_FONT_PATH);
 		mBitmapFont.load();
+
+		initMenuItemClickListeners();
+	}
+
+	private void initMenuItemClickListeners() {
+		mMenuItemClickListener = new MenuItemClickListener[mMenuItemList.length];
+		mMenuItemClickListener[0] = new MenuItemClickListener() {
+			@Override
+			public void onClick() {
+				
+			}
+		};
+		mMenuItemClickListener[1] = new MenuItemClickListener() {
+			@Override
+			public void onClick() {
+				
+			}
+		};
+		mMenuItemClickListener[2] = new MenuItemClickListener() {
+			@Override
+			public void onClick() {
+				
+			}
+		};
+		mMenuItemClickListener[3] = new MenuItemClickListener() {
+			@Override
+			public void onClick() {
+				mGameContext.getGameActivity().finish();
+			}
+		};
 	}
 
 	@Override
@@ -50,41 +117,30 @@ public class MenuGameState extends BaseGameState {
 
 		mScene = new Scene();
 		mScene.setBackground(new Background(1.0f, 1.0f, 1.0f));
+		mScene.setTouchAreaBindingOnActionDownEnabled(true);
 
 		final Engine engine = mGameContext.getEngine();
 		final Camera camera = engine.getCamera();
-		int itemInterval = 
+		int yInterval = 
 				(int)(camera.getHeight() / (mMenuItemList.length + 1));
 
 		for (int i = 0; i < mMenuItemList.length; i++) {
-			Text itemText = new Text(0, 
-					(i + 1) * itemInterval - mBitmapFont.getLineHeight() / 2, 
-					mBitmapFont, mMenuItemList[i], 
-					new TextOptions(HorizontalAlign.CENTER), 
-					mGameContext.getEngine().getVertexBufferObjectManager()) {
+			final int ci = i;
+
+			AnimatedSpriteButton button = new AnimatedSpriteButton(0, 0, 
+					mTiledButtonRegion, 
+					mEngine.getVertexBufferObjectManager()) {
+
 				@Override
-				public boolean onAreaTouched(final TouchEvent event, final float x, final float y) {
-					switch (event.getAction()) {
-					case TouchEvent.ACTION_DOWN:
-						mGameContext.getGameActivity().finish();
-						return true;
-					default:
-						return false;
-					}
+				public void onClick() {
+					mMenuItemClickListener[ci].onClick();
 				}
 			};
-
-			mScene.registerTouchArea(itemText);
-			itemText.setScale(2);
-			itemText.setX(camera.getWidth() / 2 - (itemText.getWidth()) / 2);
-
-			/*
-			ButtonSprite btn = new ButtonSprite(0, 
-					(i + 1) * itemInterval - mBitmapFont.getLineHeight() / 2,
-					engine.
-			*/
-
-			mScene.attachChild(itemText);
+			button.setText(mMenuItemList[ci], mBitmapFont);
+			button.setPosition(0, 
+					(ci + 1) * yInterval - button.getHeight() / 2);
+			mScene.registerTouchArea(button);
+			mScene.attachChild(button);
 		}
 
 		return mScene;
