19689a7f9cae38af27ba253f2e5a8d52775caeb4
Roger.LC Chuang <pharspher@gmail.com>
2012-11-24 11:00:53 +0800

    1. Add RunGameState

---
+++ src/org/ph/game/gamestate/RunGameState.java
@@ -0,0 +1,36 @@
+package org.ph.game.gamestate;
+
+import org.andengine.entity.scene.Scene;
+import org.ph.game.testgame.GameContext;
+
+public class RunGameState extends BaseGameState {
+
+	public RunGameState(GameContext context) {
+		super(context);
+	}
+
+	@Override
+	public void onCreateResources() {
+
+	}
+
+	@Override
+	public Scene onCreateScene() {
+		return null;
+	}
+
+	@Override
+	public void onResume() {
+
+	}
+
+	@Override
+	public void onPause() {
+
+	}
+
+	@Override
+	public void onDestroy() {
+
+	}
+}
